547ae605e67840798d0b1da3c9a54599b6a7c7d3
Daniel Rodak <drodak@bezkres.org>
2024-09-16 21:32:45 +0200

    Added base project structure

---
+++ src/main/java/org/bezkres/scoolee/example/ExampleServiceApplication.java
@@ -0,0 +1,13 @@
+package org.bezkres.scoolee.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bezkres/scoolee/example/ExampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bezkres.scoolee.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
